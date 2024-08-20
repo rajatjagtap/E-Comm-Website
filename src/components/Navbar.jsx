c95480326beb3944b0092cb94c2a20477f48b18e
suraj <surajjagtap1139@gmail.com>
2024-08-20 22:38:52 +0530

    navbar created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,54 @@
-import React from 'react'
-import {assets} from '../assets/frontend_assets/assets'
-import {NavLink} from 'react-router-dom'
+import React from "react";
+import { assets } from "../assets/frontend_assets/assets";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div className='flex items-center justify-between py-5 font-medium'>
-          <img src={assets.logo} className='w-36' alt="" />
+    <div className="flex items-center justify-between py-5 font-medium">
+      <img src={assets.logo} className="w-36" alt="" />
 
-         <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
-             <NavLink to='/' className='flex flex-col items-center gap-1'>
-                  <p>Home</p>
-                  <hr className='w-2/4 border-none h-[1.5px] bg-gray-700'/>
-             </NavLink>
+      <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
+        <NavLink to="/" className="flex flex-col items-center gap-1">
+          <p>HOME</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
 
-             <NavLink to='/collection' className='flex flex-col items-center gap-1'>
-                  <p>Collection</p>
-                  <hr className='w-2/4 border-none h-[1.5px] bg-gray-700'/>
-             </NavLink>
+        <NavLink to="/collection" className="flex flex-col items-center gap-1">
+          <p>COLLECTION</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
 
-             <NavLink to='/about' className='flex flex-col items-center gap-1'>
-                  <p>About</p>
-                  <hr className='w-2/4 border-none h-[1.5px] bg-gray-700'/>
-             </NavLink>
+        <NavLink to="/about" className="flex flex-col items-center gap-1">
+          <p>ABOUT</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
 
-             <NavLink to='/contact' className='flex flex-col items-center gap-1'>
-                  <p>Contact</p>
-                  <hr className='w-2/4 border-none h-[1.5px] bg-gray-700'/>
-             </NavLink>
+        <NavLink to="/contact" className="flex flex-col items-center gap-1">
+          <p>CONTACT</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
+      </ul>
 
+      <div className="flex items-center gap-6">
+        <img src={assets.search_icon} className="w-5 cursor-pointer" alt="" />
 
-         </ul>
-
+        <div className="group relative">
+          <img
+            src={assets.profile_icon}
+            className="w-5 cursor-pointer"
+            alt=""
+          />
+          <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
+            <div className="flex  flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded-lg">
+              <p className="cursor-pointer hover:text-black">My Profile</p>
+              <p className="cursor-pointer hover:text-black">Orders</p>
+              <p className="cursor-pointer hover:text-black">Logout</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
